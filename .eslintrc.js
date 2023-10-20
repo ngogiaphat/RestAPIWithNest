ef877786dd5c0c7e76d4a8adda218817e9876f2c
Samuel Seo <ngogiaphat0802@gmail.com>
2023-10-21 00:11:27 +0700

    config the rule for JS + TS language

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
 	parser: '@typescript-eslint/parser',
   parserOptions: {
-    tsconfigRootDir : __dirname, 
+    tsconfigRootDir : __dirname,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
-    'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
   root: true,
   env: {
@@ -16,12 +16,12 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    "semi": 0,
+		"@typescript-eslint/semi": "off",
+    "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/interface-name-prefix": "off",
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-		"@javascript-eslint/semi": "",
-		"@typescript-eslint/semi": "warn",
-		"semi": 0
+    "@javascript-eslint/semi": ["never"]
   },
 };
