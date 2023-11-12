025318602e25bca7c3f2d68de303723c46329bfa
Your Name <you@example.com>
2023-11-12 20:55:16 +0700

    config the rules for TS language

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,26 @@
 module.exports = {
-	parser: '@typescript-eslint/parser',
-  parserOptions: {
-    tsconfigRootDir : __dirname,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-  ],
   root: true,
+	parser: '@typescript-eslint/parser',
+  ignorePatterns: ['.eslintrc.js'],
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: ['plugin:@typescript-eslint/recommended'],
   env: {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  parserOptions: {
+    tsconfigRootDir : __dirname,
+    sourceType: 'module',
+  },
   rules: {
     "semi": 0,
     "@javascript-eslint/semi": "off",
 		"@typescript-eslint/semi": "off",
-    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off"
   },
 };
